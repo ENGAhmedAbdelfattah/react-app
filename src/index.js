9332aa129be4ba4e6a12d50ce89882a55fd8d81d
ahmed Mahmoud <rererer12@protonmail.com>
2022-03-13 21:44:53 +0200

    react-book-app

---
+++ src/index.js
@@ -0,0 +1,22 @@
+/*
+ *   Copyright (c) 2022 
+ *   All rights reserved.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './Main.css';
+
+// Add libirary
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+
+//Component
+import App from './component/App'
+
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
